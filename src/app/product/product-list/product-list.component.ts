0d849416d05a46df4dce3cc419889889b2711bce
Ricardo Alexis Hernandez Rodriguez <21000548@alumnos.utleon.edu.mx>
2024-09-20 20:53:00 -0600

    Commit Viernes 20 Sep

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { UrlCodec } from '@angular/common/upgrade';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,18 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
+
 export class ProductListComponent {
 
-productos:any[]=[
+  //*ngIf=true o false
+productos:any=[
   {
     "producto_id":1,
-    "Modelo":"Sntra",
+    "Modelo":"Sentra",
     "Descripcion":"4 puertas",
     "Precio":20000,
     "Year":2023,
     "Marca":"Kya",
-    "Color":"Azul",
-    "ImagenUrl":""
+    "Color":"Naranja",
+    "ImagenUrl":"https://th.bing.com/th/id/OIP._Xgyj7KPilfwUyCKIZVaGwAAAA?w=307&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
   },
   {
     "producto_id":2,
@@ -25,18 +28,18 @@ productos:any[]=[
     "Precio":20000,
     "Year":2023,
     "Marca":"Audi",
-    "Color":"Blanco",
-    "ImagenUrl":""
+    "Color":"Rojo",
+    "ImagenUrl":"https://th.bing.com/th/id/R.aecb55fc4993ee4332a5ac147c3a1aa8?rik=h%2bTogZhlYuUuxQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f9%2f97%2fAudi_A1.jpg&ehk=TjzJus6E3hT5EXCpzRdjWn5wx8ikVyffAZvb4VmHRlc%3d&risl=&pid=ImgRaw&r=0"
   },
   {
     "producto_id":3,
-    "Modelo":"Sntra",
-    "Descripcion":"4 puertas",
-    "Precio":20000,
+    "Modelo":"Bocho",
+    "Descripcion":"Esta chido",
+    "Precio":200,
     "Year":2023,
-    "Marca":"Kya",
-    "Color":"Azul",
-    "ImagenUrl":""
+    "Marca":"Mercedes Benz",
+    "Color":"Rosa",
+    "ImagenUrl":"https://th.bing.com/th/id/OIP.L8E-CuJTEeJVuhTANeRNDQHaE9?w=257&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
   }
 ]
 }
